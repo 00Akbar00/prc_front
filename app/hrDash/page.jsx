13ee6ea5cfc085a5c9acd6d5048ba97cf5b04e94
00Akbar00 <ma341604@gmail.com>
2025-01-01 15:43:35 +0500

    Adding Logout Button in hrDash

--- app/hrDash/page.jsx
+++ app/hrDash/page.jsx
@@ -2,16 +2,19 @@
 import React from 'react';
 import Table from '../../components/hr/table/table';
 import { styles } from '@/components/hr/table/tableStyle';
+import LogoutButton from '../../components/logoutButton'
 
 const HrDash = () => {
   return (
     <div style={styles.container}>
       <div style={styles.mainContent}>
         <h1 style={styles.tableTitle}>HR Dashboard</h1>
+        <div style={styles.logoutButtonContainer}>
+          <LogoutButton style={styles.logoutButton} hoverStyle={styles.logoutButtonHover} />
+        </div>
         <Table style={styles} />
       </div>
     </div>
   );
 };
-
 export default HrDash;
